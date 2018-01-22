6b01ee5d56ca7294404d14425cf4c935ac8bb02c
karthick-p <kr688977@dal.ca>
2018-01-22 19:24:29 -0400

    Assignment 1
    Question 3

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean button_status;
+    private Button ok;
+    private EditText entered_text;
+    TextView output_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ok = findViewById(R.id.ok);
+        entered_text = findViewById(R.id.textfield);
+        output_text = findViewById(R.id.textView);
+        button_status = false;
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (!button_status) {
+                    output_text.setText(entered_text.getText());
+                    ok.setText("Submitted");
+                    button_status = true;
+                    entered_text.setText("");
+                }
+                else {
+                    ok.setText("Submit");
+                    button_status = false;
+                }
+            }
+        });
     }
 }
